349b01ebc0080df14fa122179a9eee425b44e580
Mangiang <mangiang@orange.fr>
2022-08-27 16:15:29 -0400

    feat: color Generate button based on form state

--- lib/layout/GenerationActionsRow.dart
+++ lib/layout/GenerationActionsRow.dart
@@ -11,39 +11,51 @@ class ActionsRow extends StatefulWidget {
 }
 
 class _ActionsRowState extends State<ActionsRow> {
-  Future<void> GeneratePassword(PasswordState state) async {
-    if (state.passphrase1.isNotEmpty && state.passphrase2.isNotEmpty) {
+  bool generateEnabled = false;
+
+  Future<void> generatePassword(PasswordState state) async {
+    if (generateEnabled) {
       state.updatePassword(await PasswordGenerator.getPassword(state));
     }
   }
 
+  bool generateEnable(PasswordState state) {
+    return state.passphrase1.isNotEmpty &&
+        state.passphrase2.isNotEmpty &&
+        state.desiredLength > 0;
+  }
+
   @override
-  Widget build(BuildContext context) => Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('Delete'),
+  Widget build(BuildContext context) =>
+      Consumer<PasswordState>(builder: (context, state, child) {
+        generateEnabled = generateEnable(state);
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Delete'),
+              ),
             ),
-          ),
-          const SizedBox(width: 50),
-          Expanded(
-            child: Consumer<PasswordState>(
-              builder: (context, state, child) {
-                return ElevatedButton(
-                  onPressed: () => GeneratePassword(state),
-                  child: const Text(
-                    'Generate',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xff347d39))),
-                );
-              },
+            const SizedBox(width: 50),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: generateEnabled ? () => generatePassword(state) : null,
+                child: Text(
+                  'Generate',
+                  style: TextStyle(
+                      color: generateEnabled ? Colors.white : Colors.black),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      generateEnabled
+                          ? const Color(0xff347d39)
+                          : const Color(0xff929292)),
+                ),
+              ),
             ),
-          ),
-        ],
-      );
+          ],
+        );
+      });
 }
